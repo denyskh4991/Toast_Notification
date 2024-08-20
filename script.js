f1afe5532c68eb249d863d91912556889294cfdb
denyskh4991 <denyskh4991@gmail.com>
2024-08-20 22:03:38 +0200

    Adding 3 messages to toastBox

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let toastBox = document.getElementById('toastBox');
+let successMsg = 'Successfully submitted';
+let errorMsg = 'Please, fix the error';
+let invalidMsg = 'Invalid input, check again';
 
-function showToast () {
+function showToast (msg) {
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
 }
