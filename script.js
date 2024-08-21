28027450eb0508707e111673e8298d83bc1df80f
denyskh4991 <denyskh4991@gmail.com>
2024-08-21 12:21:12 +0200

    Adding icons to toastBox

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let toastBox = document.getElementById('toastBox');
-let successMsg = 'Successfully submitted';
-let errorMsg = 'Please, fix the error';
-let invalidMsg = 'Invalid input, check again';
+let successMsg = '<i class="fa-sharp fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please, fix the error';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
 function showToast (msg) {
     let toast = document.createElement('div');
