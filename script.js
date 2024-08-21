3d2aa29c88a760251fa5c1f42db6734e8e384b73
denyskh4991 <denyskh4991@gmail.com>
2024-08-21 12:24:43 +0200

    Styling notifications

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function showToast (msg) {
     toast.classList.add('toast');
     toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    if(msg.includes('error')) {
+        toast.classList.add('error');
+    }
+
+    if(msg.includes('Invalid')) {
+        toast.classList.add('invalid');
+    }
 }
